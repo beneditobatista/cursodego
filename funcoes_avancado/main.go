7d1e5b302d67fa3df42f050936b566d6f0e43a0d
Vinicius Senger <vinicius@globalcode.com.br>
2018-01-13 13:13:43 -0200

    Funçoes avancadas

--- funcoes_avancado/main.go
+++ funcoes_avancado/main.go
@@ -14,4 +14,10 @@ func main() {
 	resultado = matematica.Calculo(matematica.Soma, 5, 4)
 	fmt.Printf("O resultado da soma foi: %d\r\n", resultado)
 
+	resultado = matematica.Calculo(matematica.Divisor, 12, 3)
+	fmt.Printf("O resultado da Divisao foi: %d\r\n", resultado)
+
+	resultado, resto := matematica.DivisorComResto(12, 5)
+	fmt.Printf("O resultado da Divisao foi: %d e o resto: %d\r\n", resultado, resto)
+
 }
